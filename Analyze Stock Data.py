eaed70d853297a1925fa862f20ef1a579106faa6
sayujshah <64810038+sayujshah@users.noreply.github.com>
2020-06-24 21:50:38 -0500

    Update Analyze Stock Data.py

--- Analyze Stock Data.py
+++ Analyze Stock Data.py
@@ -6,7 +6,6 @@ Created on Mon Jun 22 12:22:28 2020
 """
 
 import pandas as pd
-from pandas.util.testing import assert_frame_equal
 import pandas_datareader.data as web
 from datetime import datetime
 import numpy as np
@@ -16,9 +15,8 @@ import matplotlib.pyplot as plt
 today = datetime.strftime(datetime.today(), "%Y-%m-%d")
 
 # Create the function to extract data from a specific data sources (CSV, API, JSON, etc.)
-def get_symbols(symbols,data_source, begin_date=None,end_date=None):
+def get_symbols(symbols,data_source, begin_date, end_date=today):
     """
-
     Parameters
     ----------
     symbols : stock ticker you want to pull data for
@@ -26,22 +24,19 @@ def get_symbols(symbols,data_source, begin_date=None,end_date=None):
     begin_date : start of lookback period/start point for the timeframe you want data for (default is None)
     end_date : end of lookback period/end point for the timeframe you want data for (default is None)
         DESCRIPTION. The default is None.
-
     Returns: a dataframe of stock data for the specified ticker that includes opening, high, low, and closing price, as well as stock volume
     -------
-
     """
     
-    out = pd.DataFrame()
+    out = []
     for symbol in symbols:
-        # Make sure to insert your own API key in place for "api_key"
-        df = web.DataReader(symbol, data_source,begin_date, end_date, api_key="""<insert your API key here>""")\
+        df = web.DataReader(symbol, data_source, begin_date, end_date, api_key='K45BwXh1hBWjQMVjz7d5')\
         [['AdjOpen','AdjHigh','AdjLow','AdjClose','AdjVolume']].reset_index()
-        df.columns = ['Date','Open','High','Low',symbol,'Volume'] #my convention: always lowercase
-        df['Symbol'] = symbol # add a new column which contains the symbol so we can keep multiple symbols in the same dataframe
+        df.columns = ['Date','Open','High','Low',symbol,'Volume']
         df = df[::-1].reset_index()
-        out = pd.concat([out,df],axis=0) #stacks on top of previously collected data
-    return out.sort_index()
+        out.append(df.sort_index()) #stacks on top of previously collected data
+    return out
+
 
 def input_ticker():
     """
@@ -63,16 +58,16 @@ def input_ticker():
 # User-specified stock ticker will be the "symbols" input for the "get_symbols" function
 symbol = input_ticker()
 
-# Call the function and assign the returned dataframe object to a variable. I am using Quandl here but feel free to use any data source you are comfortable with
+# Call the function and assign the returned dataframe object to a variable
 df = get_symbols(symbol,data_source='quandl',\
-                     begin_date='2009-12-31',end_date=today)
+                     begin_date='2009-12-31', end_date=today)
 
 # Plot the stock price data for each stock symbol
-for i in symbol:
+for i,v in enumerate(symbol):
     plt.figure(figsize=(14, 5), dpi=100)
-    plt.plot(df['Date'],df[i], label=i + ' stock')
+    plt.plot(df[i]['Date'],df[i][v], label=f'{v} stock')
     plt.xlabel('Date')
     plt.ylabel('USD')
-    plt.title('Figure 2: ' + i + ' stock price')
+    plt.title(f'Figure {i+1}: {v} stock price')
     plt.legend()
     plt.show()
