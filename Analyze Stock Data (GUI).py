ae842a56a5d45e1a92a1af829bfc57f9a2100600
sayujshah <64810038+sayujshah@users.noreply.github.com>
2020-06-27 19:06:32 -0500

    Update Analyze Stock Data (GUI).py

--- Analyze Stock Data (GUI).py
+++ Analyze Stock Data (GUI).py
@@ -27,7 +27,22 @@ today = datetime.strftime(datetime.today(), "%Y-%m-%d")
 
 class StockAnalysis(tk.Frame):
     
-    def __init__(self, master=None):    
+    """
+    
+    This class creates a GUI that features the ability for a user to choose
+    any stock and graph its price data. It also allows for a user to choose
+    a stock indicator from a provided dropdown menu in order to conduct
+    the proper analysis for a stock and determine whether to buy or sell
+    
+    """
+    
+    def __init__(self, master=None):
+        
+        """
+    
+        Initialization function that sets up the GUI
+    
+        """
         self.tickers = []
         self.input_list = []
         
@@ -36,6 +51,15 @@ class StockAnalysis(tk.Frame):
         self.createWidgets()
         
     def createWidgets(self):
+        
+        """
+        
+        Function that deploys all the widgets that will be seen in the GUI
+        window, as well as the action that will occur upon interacting with
+        the widget
+        
+        """
+        
         master = tk.Tk()
         master.geometry("")
         self.variable = tk.StringVar(master)
@@ -47,30 +71,31 @@ class StockAnalysis(tk.Frame):
         self.entrybox.grid(row=1, column=0, sticky='NSEW')
         
         self.tickerButton = tk.Button(master, text='Graph', command=self.graphStock)
-        self.tickerButton.grid(row=2, column=0, sticky='NSEW')
+        self.tickerButton.grid(row=1, column=1, sticky='NSEW')
     
         indicator_graph_title = tk.Label(master, text='Indicator').grid(row=4, column=0)
         self.indicatorDropdown = tk.OptionMenu(master, self.variable, *OPTIONS)
         self.indicatorDropdown.grid(row=5, column=0, sticky='NSEW')
         
-        self.indicatorButton = tk.Button(master, text='Graph')
-        self.indicatorButton['command'] = self.graphIndicator
-        self.indicatorButton.grid(row=6, column=0, sticky='NSEW')
+        self.indicatorButton = tk.Button(master, text='Graph', command=self.graphIndicator)
+        self.indicatorButton.grid(row=5, column=1, sticky='NSEW')
         
         self.clearButton1 = tk.Button(master, text='Clear', command=self.clearGraph1)
-        self.clearButton1.grid(row=2, column=1, sticky='NSEW')
+        self.clearButton1.grid(row=1, column=2, sticky='NSEW')
         
         self.clearButton2 = tk.Button(master, text='Clear', command=self.clearGraph2)
-        self.clearButton2.grid(row=5, column=1, sticky='NSEW')
+        self.clearButton2.grid(row=5, column=2, sticky='NSEW')
         
-        self.canvas1 = tk.Canvas(master, width=1000, height=250)
-        self.canvas1.grid(row=3, column=0, columnspan=3)
+        self.canvas1 = tk.Canvas(master, width=1000, height=300)
+        self.canvas1.grid(row=3, column=0, columnspan=4)
         
-        self.canvas2 = tk.Canvas(master, width=1000, height=250)
-        self.canvas2.grid(row=7, column=0, columnspan=3)
+        self.canvas2 = tk.Canvas(master, width=1000, height=300)
+        self.canvas2.grid(row=7, column=0, columnspan=4)
     
-    # Create the function to extract data from a specific data sources (CSV, API, JSON, etc.)
+    # The following function extracts stock data from a specific data source
+    # (CSV, API, JSON, etc.)
     def getSymbols(self, symbol, data_source, begin_date=None, end_date=None):
+        
         """
     
         Parameters
@@ -85,32 +110,44 @@ class StockAnalysis(tk.Frame):
         -------
     
         """
+        
         out = pd.DataFrame()
         df = web.DataReader(symbol, data_source,begin_date, end_date, api_key="""INSERT YOUR API KEY HERE""")\
         [['AdjOpen','AdjHigh','AdjLow','AdjClose','AdjVolume']].reset_index()
-        df.columns = ['Date','Open','High','Low',symbol,'Volume'] #my convention: always lowercase
-        df['Symbol'] = symbol # add a new column which contains the symbol so we can keep multiple symbols in the same dataframe
+        df.columns = ['Date','Open','High','Low',symbol,'Volume']
+        df['Symbol'] = symbol
         df = df[::-1].reset_index()
-        out = pd.concat([out,df],axis=0) #stacks on top of previously collected data
+        out = pd.concat([out,df],axis=0)
         return out.sort_index()
 
 
     def graphStock(self):
+        
+        """
+        
+        Function that graphs the user-inputted stock tickers from the
+        provided entry box
+        
+        """
+        
         try:
             self.can1.get_tk_widget().pack_forget()
         except AttributeError:
             pass
        
-        # User-specified stock ticker will be the "symbols" input for the "getSymbols" function
+        # User-specified stock ticker will be the "symbols" input for the
+        # "getSymbols" function
         user_entry = self.entrybox.get()
         self.input_list.append(user_entry)
         
-        fig = Figure(figsize=(10,2), dpi=100) 
+        fig = Figure(figsize=(12,3), dpi=100) 
         graph = fig.add_subplot(111)
         
         i = 0
+        
+        # The following for loop pulls stock data for the chosen stocks and
+        # creates a dataframe
         for symbol in self.input_list:
-            # Call the function and assign the returned dataframe object to a variable
             self.df = self.getSymbols(symbol,data_source='quandl',\
                                  begin_date='2009-12-31',end_date=today)
                 
@@ -119,14 +156,18 @@ class StockAnalysis(tk.Frame):
             graph.set_title(f'{", ".join(self.input_list)} stock price')
             graph.set_ylabel('USD')
             i += 1
-            
+        
+        # The following creates the graph in the blank canvas
         self.can1 = FigureCanvasTkAgg(fig, self.canvas1)
         self.can1.get_tk_widget().pack(side="top",fill='both',expand=True)
         
     def graphIndicator(self):
+        
         """
         
-        Function that allows user to input what technical analysis indicator they would like to use for their analysis
+        Function that graphs thh user-chosen indicator from the provided
+        dropdown. All indicators follow default parameters (see TA-Lib
+        documentation for further information)
         
         """
         
@@ -136,21 +177,19 @@ class StockAnalysis(tk.Frame):
             pass
         
         indicator = self.variable.get()
+        # Dictionary of all functions from the TA-Lib library
         indicator_dict = {'HT_DCPERIOD': HT_DCPERIOD, 'HT_DCPHASE': HT_DCPHASE, 'HT_PHASOR': HT_PHASOR, 'HT_SINE': HT_SINE, 'HT_TRENDMODE': HT_TRENDMODE, 'ADD': ADD, 'DIV': DIV, 'MAX': MAX, 'MAXINDEX': MAXINDEX, 'MIN': MIN, 'MININDEX': MININDEX, 'MINMAX': MINMAX, 'MINMAXINDEX': MINMAXINDEX, 'MULT': MULT, 'SUB': SUB, 'SUM': SUM, 'ACOS': ACOS, 'ASIN': ASIN, 'ATAN': ATAN, 'CEIL': CEIL, 'COS': COS, 'COSH': COSH, 'EXP': EXP, 'FLOOR': FLOOR, 'LN': LN, 'LOG10': LOG10, 'SIN': SIN, 'SINH': SINH, 'SQRT': SQRT, 'TAN': TAN, 'TANH': TANH, 'ADX': ADX, 'ADXR': ADXR, 'APO': APO, 'AROON': AROON, 'AROONOSC': AROONOSC, 'BOP': BOP, 'CCI': CCI, 'CMO': CMO, 'DX': DX, 'MACD': MACD, 'MACDEXT': MACDEXT, 'MACDFIX': MACDFIX, 'MFI': MFI, 'MINUS_DI': MINUS_DI, 'MINUS_DM': MINUS_DM, 'MOM': MOM, 'PLUS_DI': PLUS_DI, 'PLUS_DM': PLUS_DM, 'PPO': PPO, 'ROC': ROC, 'ROCP': ROCP, 'ROCR': ROCR, 'ROCR100': ROCR100, 'RSI': RSI, 'STOCH': STOCH, 'STOCHF': STOCHF, 'STOCHRSI': STOCHRSI, 'TRIX': TRIX, 'ULTOSC': ULTOSC, 'WILLR': WILLR, 'BBANDS': BBANDS, 'DEMA': DEMA, 'EMA': EMA, 'HT_TRENDLINE': HT_TRENDLINE, 'KAMA': KAMA, 'MA': MA, 'MAMA': MAMA, 'MAVP': MAVP, 'MIDPOINT': MIDPOINT, 'MIDPRICE': MIDPRICE, 'SAR': SAR, 'SAREXT': SAREXT, 'SMA': SMA, 'T3': T3, 'TEMA': TEMA, 'TRIMA': TRIMA, 'WMA': WMA, 'CDL2CROWS': CDL2CROWS, 'CDL3BLACKCROWS': CDL3BLACKCROWS, 'CDL3INSIDE': CDL3INSIDE, 'CDL3LINESTRIKE': CDL3LINESTRIKE, 'CDL3OUTSIDE': CDL3OUTSIDE, 'CDL3STARSINSOUTH': CDL3STARSINSOUTH, 'CDL3WHITESOLDIERS': CDL3WHITESOLDIERS, 'CDLABANDONEDBABY': CDLABANDONEDBABY, 'CDLADVANCEBLOCK': CDLADVANCEBLOCK, 'CDLBELTHOLD': CDLBELTHOLD, 'CDLBREAKAWAY': CDLBREAKAWAY, 'CDLCLOSINGMARUBOZU': CDLCLOSINGMARUBOZU, 'CDLCONCEALBABYSWALL': CDLCONCEALBABYSWALL, 'CDLCOUNTERATTACK': CDLCOUNTERATTACK, 'CDLDARKCLOUDCOVER': CDLDARKCLOUDCOVER, 'CDLDOJI': CDLDOJI, 'CDLDOJISTAR': CDLDOJISTAR, 'CDLDRAGONFLYDOJI': CDLDRAGONFLYDOJI, 'CDLENGULFING': CDLENGULFING, 'CDLEVENINGDOJISTAR': CDLEVENINGDOJISTAR, 'CDLEVENINGSTAR': CDLEVENINGSTAR, 'CDLGAPSIDESIDEWHITE': CDLGAPSIDESIDEWHITE, 'CDLGRAVESTONEDOJI': CDLGRAVESTONEDOJI, 'CDLHAMMER': CDLHAMMER, 'CDLHANGINGMAN': CDLHANGINGMAN, 'CDLHARAMI': CDLHARAMI, 'CDLHARAMICROSS': CDLHARAMICROSS, 'CDLHIGHWAVE': CDLHIGHWAVE, 'CDLHIKKAKE': CDLHIKKAKE, 'CDLHIKKAKEMOD': CDLHIKKAKEMOD, 'CDLHOMINGPIGEON': CDLHOMINGPIGEON, 'CDLIDENTICAL3CROWS': CDLIDENTICAL3CROWS, 'CDLINNECK': CDLINNECK, 'CDLINVERTEDHAMMER': CDLINVERTEDHAMMER, 'CDLKICKING': CDLKICKING, 'CDLKICKINGBYLENGTH': CDLKICKINGBYLENGTH, 'CDLLADDERBOTTOM': CDLLADDERBOTTOM, 'CDLLONGLEGGEDDOJI': CDLLONGLEGGEDDOJI, 'CDLLONGLINE': CDLLONGLINE, 'CDLMARUBOZU': CDLMARUBOZU, 'CDLMATCHINGLOW': CDLMATCHINGLOW, 'CDLMATHOLD': CDLMATHOLD, 'CDLMORNINGDOJISTAR': CDLMORNINGDOJISTAR, 'CDLMORNINGSTAR': CDLMORNINGSTAR, 'CDLONNECK': CDLONNECK, 'CDLPIERCING': CDLPIERCING, 'CDLRICKSHAWMAN': CDLRICKSHAWMAN, 'CDLRISEFALL3METHODS': CDLRISEFALL3METHODS, 'CDLSEPARATINGLINES': CDLSEPARATINGLINES, 'CDLSHOOTINGSTAR': CDLSHOOTINGSTAR, 'CDLSHORTLINE': CDLSHORTLINE, 'CDLSPINNINGTOP': CDLSPINNINGTOP, 'CDLSTALLEDPATTERN': CDLSTALLEDPATTERN, 'CDLSTICKSANDWICH': CDLSTICKSANDWICH, 'CDLTAKURI': CDLTAKURI, 'CDLTASUKIGAP': CDLTASUKIGAP, 'CDLTHRUSTING': CDLTHRUSTING, 'CDLTRISTAR': CDLTRISTAR, 'CDLUNIQUE3RIVER': CDLUNIQUE3RIVER, 'CDLUPSIDEGAP2CROWS': CDLUPSIDEGAP2CROWS, 'CDLXSIDEGAP3METHODS': CDLXSIDEGAP3METHODS, 'AVGPRICE': AVGPRICE, 'MEDPRICE': MEDPRICE, 'TYPPRICE': TYPPRICE, 'WCLPRICE': WCLPRICE, 'BETA': BETA, 'CORREL': CORREL, 'LINEARREG': LINEARREG, 'LINEARREG_ANGLE': LINEARREG_ANGLE, 'LINEARREG_INTERCEPT': LINEARREG_INTERCEPT, 'LINEARREG_SLOPE': LINEARREG_SLOPE, 'STDDEV': STDDEV, 'TSF': TSF, 'VAR': VAR, 'ATR': ATR, 'NATR': NATR, 'TRANGE': TRANGE, 'AD': AD, 'ADOSC': ADOSC, 'OBV': OBV}
         keys_list = list(indicator_dict)
         
         legend = []
         
-        openprice = self.df['Open']
-        high = self.df['High']
-        low = self.df['Low']
-        volume= self.df['Volume']
-        periods = 30
-        
-        fig = Figure(figsize=(10,2), dpi=100) 
+        fig = Figure(figsize=(12,2.5), dpi=100) 
         graph = fig.add_subplot(111)
             
         i = 0
+        
+        # The following for loop pulls stock data for the chosen stocks and
+        # creates a dataframe
         for symbol in self.input_list:
             self.df = self.getSymbols(symbol,data_source='quandl',\
                                  begin_date='2009-12-31',end_date=today)
@@ -160,6 +199,10 @@ class StockAnalysis(tk.Frame):
             volume= self.df['Volume']
             periods = 30
             close = self.df[symbol]
+            
+            # The following if statements are necessary to pull the chosen
+            # indicator and plot the line based on its specific function
+            # parameters
             
             # CYCLE INDICATORS
             if indicator in keys_list[0:5]:
@@ -219,7 +262,7 @@ class StockAnalysis(tk.Frame):
             elif indicator in keys_list[34]:
                 ind = indicator_dict.get(indicator)
                 aroondown, aroonup = ind(high, low)
-                aroondown.plot(kind='line', legend=True, ax=graph, color=f'C{i}', fontsize=10)
+                aroondown.plot(kind='line', legend=True, ax=graph, color=f'C{i+1}', fontsize=10)
                 aroonup.plot(kind='line', legend=True, ax=graph, color=f'C{i}', fontsize=10)
                 legend.append(symbol)
                 i += 1
@@ -256,8 +299,8 @@ class StockAnalysis(tk.Frame):
             elif indicator in keys_list[40:43]:
                 ind = indicator_dict.get(indicator)
                 macd, macdsignal, macdhist = ind(close)
-                # macd.plot(kind='line', legend=True, ax=graph, color=f'C{i}', fontsize=10)
-                # macdsignal.plot(kind='line', legend=True, ax=graph, color=f'C{i}', fontsize=10)
+                # macd.plot(kind='line', legend=True, ax=graph, color=f'C{i+2}', fontsize=10)
+                # macdsignal.plot(kind='line', legend=True, ax=graph, color=f'C{i+1}', fontsize=10)
                 macdhist.plot(kind='line', legend=True, ax=graph, color=f'C{i}', fontsize=10)
                 legend.append(symbol)
                 i += 1
@@ -307,21 +350,21 @@ class StockAnalysis(tk.Frame):
                 ind = indicator_dict.get(indicator)
                 slowk, slowd = ind(high, low, close)
                 slowk.plot(kind='line', legend=True, ax=graph, color=f'C{i}', fontsize=10)
-                slowd.plot(kind='line', legend=True, ax=graph, color=f'C{i}', fontsize=10)
+                slowd.plot(kind='line', legend=True, ax=graph, color=f'C{i+1}', fontsize=10)
                 legend.append(symbol)
                 i += 1
             elif indicator in keys_list[56]:
                 ind = indicator_dict.get(indicator)
                 fastk, fastd = ind(high, low, close)
                 fastk.plot(kind='line', legend=True, ax=graph, color=f'C{i}', fontsize=10)
-                fastd.plot(kind='line', legend=True, ax=graph, color=f'C{i}', fontsize=10)
+                fastd.plot(kind='line', legend=True, ax=graph, color=f'C{i+1}', fontsize=10)
                 legend.append(symbol)
                 i += 1
             # elif indicator in keys_list[57]:
             #     ind = indicator_dict.get(indicator)
             #     fastk, fastd = ind(high, low, close)
             #     fastk.plot(kind='line', legend=True, ax=graph, color=f'C{i}', fontsize=10)
-            #     fastd.plot(kind='line', legend=True, ax=graph, color=f'C{i}', fontsize=10)
+            #     fastd.plot(kind='line', legend=True, ax=graph, color=f'C{i+1}', fontsize=10)
             #     legend.append(symbol)
             #     i += 1
             elif indicator in keys_list[58]:
@@ -338,12 +381,12 @@ class StockAnalysis(tk.Frame):
                 i += 1
             
             # OVERLAP STUDIES FUNCTIONS
-            elif indicator in keys_list[56]:
+            elif indicator in keys_list[61]:
                 ind = indicator_dict.get(indicator)
                 upperband, middleband, lowerband = ind(close)
-                upperband.plot(kind='line', legend=True, ax=graph, color=f'C{i}', fontsize=10)
+                upperband.plot(kind='line', legend=True, ax=graph, color=f'C{i+1}', fontsize=10)
                 middleband.plot(kind='line', legend=True, ax=graph, color=f'C{i}', fontsize=10)
-                lowerband.plot(kind='line', legend=True, ax=graph, color=f'C{i}', fontsize=10)
+                lowerband.plot(kind='line', legend=True, ax=graph, color=f'C{i+1}', fontsize=10)
                 legend.append(symbol)
                 i += 1
             elif indicator in keys_list[62:68]:
@@ -446,17 +489,32 @@ class StockAnalysis(tk.Frame):
             graph.set_xlabel('Date')
             graph.legend(legend)
                 
-        
+        # The following creates the graph in the blank canvas
         self.can2 = FigureCanvasTkAgg(fig, self.canvas2)
         self.can2.get_tk_widget().pack(side="top",fill='both',expand=True)
     
     def clearGraph1(self):
+        
+        """
+        
+        Function that clears the stock graph upon clicking the "Clear" button
+        
+        """
+        
         self.can1.get_tk_widget().pack_forget()
         self.input_list = []
     
     def clearGraph2(self):
-        self.can2.get_tk_widget().pack_forget()
+        
+        """
+        
+        Function that clears the indicator graph upon clicking the "Clear"
+        button
+        
+        """
+        
+        self.can2.get_tk_widget().pack_forget()    
     
-    
+# The following initiates the program
 app = StockAnalysis(tk.Tk())
 app.mainloop()
